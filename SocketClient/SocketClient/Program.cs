7dfdb3254f627ca0f1d9b678c8773824c5479012
[MahammadAsgar] <mahammadasgarli59@gmail.com>
2023-06-03 23:41:17 +0400

    Simple Socket Network Programming

---
+++ SocketClient/SocketClient/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace SocketClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ipAddr = null;
+            try
+            {
+                Console.WriteLine("Socket Client");
+                Console.WriteLine("Enter Ip Adress");
+                string strIp = Console.ReadLine();
+                int portInput = 0;
+
+                Console.WriteLine("Enter port");
+                string strPort = Console.ReadLine();
+                if (!IPAddress.TryParse(strIp, out ipAddr))
+                {
+                    Console.WriteLine("invalid Ip adress");
+                    return;
+                }
+
+                if (!int.TryParse(strPort.Trim(), out portInput))
+                {
+                    Console.WriteLine("invalid port");
+                    return;
+                }
+                Console.WriteLine(string.Format("IpAdress: {0} - Port: {1}", strIp.ToString(), portInput));
+                client.Connect(ipAddr, portInput);
+                Console.WriteLine("connected to the server. text ex to close");
+                string inputData;
+                while (true)
+                {
+                    inputData = Console.ReadLine();
+                    if (inputData.Equals("ex"))
+                    {
+                        break;
+                    }
+                    byte[] buffSend = Encoding.ASCII.GetBytes(inputData);
+                    client.Send(buffSend);
+
+                    byte[] buffRecieve = new byte[128];
+                    int recivedBytes = client.Receive(buffRecieve);
+                    Console.WriteLine("Recived Data: " + Encoding.ASCII.GetString(buffRecieve, 0, recivedBytes));
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                client.Shutdown(SocketShutdown.Both);
+                client.Close();
+                client.Dispose();
+            }
+        }
+    }
+}
---
+++ SocketServer/SocketServer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SocketServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ipAddr = IPAddress.Any;
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, 23000);
+            listener.Bind(ipEndPoint);
+            Console.WriteLine("About to accept to incoming connection");
+            listener.Listen(5);
+            Socket client = listener.Accept();
+            Console.WriteLine("Client connected " + client.ToString() + "Ip Adress: " + client.RemoteEndPoint.ToString());
+            byte[] buffer = new byte[128];
+            while (true)
+            {
+                int numberOfRecivedByte = 0;
+                numberOfRecivedByte = client.Receive(buffer);
+                Console.WriteLine("Number of recived bytes: " + numberOfRecivedByte);
+                string data = Encoding.UTF8.GetString(buffer);
+                Console.WriteLine("Data send by client: " + data);
+                client.Send(buffer);
+
+                if (data == "x")
+                {
+                    break;
+                }
+                Array.Clear(buffer, 0, numberOfRecivedByte);
+                numberOfRecivedByte = 0;
+            }
+        }
+    }
+}
